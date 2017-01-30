0a0b8c1975b930a29eb731574bcff95ba79222e8
Brian Mix <mix.brian@gmail.com>
2017-01-29 19:44:21 -0600

    Add lookup by place_id

--- src/Jcf/Geocode/Geocode.php
+++ src/Jcf/Geocode/Geocode.php
@@ -91,5 +91,42 @@ class Geocode
 		}
 
     }
+	
+    public function placeId($id)
+    {
+        if (empty($id)) {
+            throw new Exceptions\EmptyArgumentsException('Empty arguments.');
+        }
+
+        $params = ['place_id' => $id];
+
+        if (!empty($this->apiKey)) {
+            $params['key'] = $this->apiKey;
+        }
+
+        if (!empty($this->language)) {
+            $params['language'] = $this->language;
+        }
+
+        $client = new \GuzzleHttp\Client();
+        $response = json_decode($client->get('https://maps.googleapis.com/maps/api/geocode/json', [
+            'query' => $params
+        ])->getBody());
+
+        # check for status in the response
+        switch( $response->status )
+        {
+
+            case "ZERO_RESULTS": # indicates that the geocode was successful but returned no results. This may occur if the geocoder was passed a non-existent address.
+            case "OVER_QUERY_LIMIT": # indicates that you are over your quota.
+            case "REQUEST_DENIED": # indicates that your request was denied.
+            case "INVALID_REQUEST": # generally indicates that the query (address, components or latlng) is missing.
+            case "UNKNOWN_ERROR":
+                return false;
+
+            case "OK": # indicates that no errors occurred; the address was successfully parsed and at least one geocode was returned.
+                return new Response($response);
+        }
+    }
 
 }

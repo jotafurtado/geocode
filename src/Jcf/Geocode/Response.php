b50301c71b169033bfcef57d0ed754cf5a80be90
Niellles <nt.vdgalien@gmail.com>
2018-06-28 21:47:18 +0200

    Added magic __get, values can now be accessed as properties

--- src/Jcf/Geocode/Response.php
+++ src/Jcf/Geocode/Response.php
@@ -1,46 +1,55 @@
-<?php 
+<?php
 namespace Jcf\Geocode;
 
 class Response
 {
-	public function __construct($response)
-	{
-		$this->response = $response->results[0];
-	}
+    public function __construct($response)
+    {
+        $this->response = $response->results[0];
+    }
 
-	public function raw()
-	{
-		return (object) $this->response;
-	}
+    public function raw()
+    {
+        return (object)$this->response;
+    }
 
-	public function formattedAddress()
-	{
-		return $this->response->formatted_address;
-	}
+    public function formattedAddress()
+    {
+        return $this->response->formatted_address;
+    }
 
-	public function latitude()
-	{
-		return $this->response->geometry->location->lat;
-	}
+    public function latitude()
+    {
+        return $this->response->geometry->location->lat;
+    }
 
-	public function longitude()
-	{
-		return $this->response->geometry->location->lng;
-	}
+    public function longitude()
+    {
+        return $this->response->geometry->location->lng;
+    }
 
-	public function locationType()
-	{
-		return $this->response->geometry->location_type;
-	}
-	
-	public function postalCode()
-    	{
-		foreach ($this->response->address_components as $component) {
-		    if (isset($component->types) && in_array('postal_code', $component->types)) {
-			return $component->long_name;
-		    }
-		}
+    public function locationType()
+    {
+        return $this->response->geometry->location_type;
+    }
 
-        	return false;
-    	}
+    public function postalCode()
+    {
+        foreach ($this->response->address_components as $component) {
+            if (isset($component->types) && in_array('postal_code', $component->types)) {
+                return $component->long_name;
+            }
+        }
+
+        return false;
+    }
+
+    public function __get($name)
+    {
+        if (method_exists($this, $name) && !in_array($name, ['raw', '__construct'])) {
+            return $this->{$name}(); // Consider using: call_user_func
+        }
+
+        throw new \Exception('Property doesn\'t exist (that is: method not implemented).');
+    }
 }
